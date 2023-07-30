28e3e698fc97a5482078fded3a7f8fd2e2c7f2b7
James <james@jamesnzl.xyz>
2023-07-30 18:36:04 +1200

    docs(firmware): :bulb: add doxygen comments to command parser (#11)

--- firmware/CS301_Class.cydsn/commands.c
+++ firmware/CS301_Class.cydsn/commands.c
@@ -7,7 +7,7 @@ CommandEnumeration matchCommand(char *command)
 {
 	for (uint8_t i = 0; COMMAND_LIST[i].enumeration != COMMAND_NOT_FOUND; i++)
 	{
-		uint8_t commandLength = strlen(COMMAND_LIST[i].command);
+		uint8_t commandLength = strlen(COMMAND_LIST[i].keyword);
 
 		// Check that the incoming command is the correct length
 		if (command[commandLength] != '\0')
@@ -15,7 +15,7 @@ CommandEnumeration matchCommand(char *command)
 			return COMMAND_NOT_FOUND;
 		}
 
-		if (strncmp(command, COMMAND_LIST[i].command, commandLength) == 0)
+		if (strncmp(command, COMMAND_LIST[i].keyword, commandLength) == 0)
 		{
 			return COMMAND_LIST[i].enumeration;
 		}
--- firmware/CS301_Class.cydsn/commands.h
+++ firmware/CS301_Class.cydsn/commands.h
@@ -5,6 +5,10 @@
 
 #define COMMAND_DELIMITER " "
 
+/**
+ * @brief The `switch`-able enumeration value that is mapped to by a command string.
+ */
+// TODO: what is the size of each enum member?
 typedef enum CommandEnumeration
 {
 	COMMAND_CHANGE_DUTY,
@@ -12,18 +16,36 @@ typedef enum CommandEnumeration
 	COMMAND_NOT_FOUND,
 } CommandEnumeration;
 
+/**
+ * @brief A command that is parsable by the robot.
+ */
 typedef struct Command
 {
-	char *command;
+	/**
+	 * @brief The command keyword as a null-terminated C string.
+	 */
+	char *keyword;
+	/**
+	 * @brief The command enumeration.
+	 */
 	CommandEnumeration enumeration;
 } Command;
 
+/**
+ * @brief The list of all recognised commands.
+ */
 static const Command COMMAND_LIST[3] = {
 	{ "CHG_DUTY", COMMAND_CHANGE_DUTY },
 	{ "CHG_DIR", COMMAND_CHANGE_DIRECTION },
 	{ NULL, COMMAND_NOT_FOUND },
 };
 
+/**
+ * @brief Attempt to match a null-terminated C string to a recognised command enumeration.
+ *
+ * @param command The null-terminated C string to parse as a command keyword.
+ * @return CommandEnumeration The matched command enumeration, or COMMAND_NOT_FOUND if unsuccessful.
+ */
 CommandEnumeration match_command(char *command);
 
 #endif
