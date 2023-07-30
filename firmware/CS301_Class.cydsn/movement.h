80303d7a59f8ff2a1249e2511ef7f2c660e37829
bcho892 <AKLbc70@gmail.com>
2023-07-30 20:52:30 +1200

    doc: add documentation for turning defines

--- firmware/CS301_Class.cydsn/movement.h
+++ firmware/CS301_Class.cydsn/movement.h
@@ -4,6 +4,9 @@
 #include <stddef.h>
 #include <stdint.h>
 
+/**
+ * @brief The possible movement directions for the robot.
+ */
 typedef enum Direction
 {
     FORWARD,
@@ -11,7 +14,19 @@ typedef enum Direction
 } Direction;
 
 // Turning
+
+/**
+ * @brief Turns the robot right (on the spot) by angle degrees
+ *
+ * @param angle The angle (in degrees) to turn right from the current position
+ */
 void turn_left(uint8_t angle);
+
+/**
+ * @brief Turns the robot left (on the spot) by angle degrees
+ *
+ * @param angle The angle (in degrees) to turn left from the current position
+ */
 void turn_right(uint8_t angle);
 
 // Direction
