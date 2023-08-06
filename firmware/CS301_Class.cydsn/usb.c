11c55ec75d8e1eb3a9540f85cc3a5624c405d6b6
James <james@jamesnzl.xyz>
2023-08-06 13:46:06 +1200

    docs(firmware): :bulb: add doxygen comments for usbuart module (#20)

--- firmware/CS301_Class.cydsn/usb.c
+++ firmware/CS301_Class.cydsn/usb.c
@@ -9,8 +9,6 @@ void USB_put_string(char *s)
 	while (USBUART_CDCIsReady() == 0)
 		;
 
-	// ! Assumes that *s is a string with allocated space >= 64 chars
-	// ? Since USB implementation restricts data packets to 64 chars, this function truncates the length to 62 chars (63rd char is a '!')
 	s[63] = '\0';
 	s[62] = '!';
 
@@ -30,10 +28,6 @@ void USB_put_char(char c)
 
 void USB_get_input(void)
 {
-	// handles input at terminal, echos it back to the terminal
-	// turn echo OFF, key emulation: only CR
-	// entered string is made available in 'line' and 'flag_KB_string' is set
-
 	static uint8 usbStarted = FALSE;
 	static uint16 usbBufCount = 0;
 	uint8 c;
--- firmware/CS301_Class.cydsn/usb.h
+++ firmware/CS301_Class.cydsn/usb.h
@@ -13,11 +13,36 @@
 extern uint8 flag_KB_string = 0;
 
 char displaystring[BUF_SIZE] = "CS301 2016\n";
-char line[BUF_SIZE], entry[BUF_SIZE];
+char USB_input[BUF_SIZE];
+char entry[BUF_SIZE];
 uint8 usbBuffer[BUF_SIZE];
 
+/**
+ * @brief Get input from the terminal.
+ *
+ * @note - Turn echo `OFF`
+ * @note - Use `CR` line endings
+ *
+ * @returns void The input string is made available in `line`, and `flag_KB_string` is set.
+ */
 void USB_get_input(void);
-void USB_put_string(char *s);
-void USB_put_char(char c);
+
+/**
+ * @brief Put a string to the terminal.
+ *
+ * @warning `string` must be C-string that has allocated space for at least 64 characters.
+ *
+ * @note The USB implementation restricts data packets to 64 characters, so this function truncates the length to 62 characters (where the 63rd character is a `'!'`).
+ *
+ * @param string The string to put to the terminal.
+ */
+void USB_put_string(char *string);
+
+/**
+ * @brief Put a character to the terminal.
+ *
+ * @param character The character to put to the terminal.
+ */
+void USB_put_char(char character);
 
 #endif
