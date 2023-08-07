b8622e3fd127dd896af9e8cc15c8aeaebe4d0a6f
James <james@jamesnzl.xyz>
2023-08-07 18:16:42 +1200

    feat(firmware): :sparkles: implement rolling buffer (#25)

---
+++ firmware/Team01.cydsn/lib/RollingBuffer/RollingBuffer.c
@@ -0,0 +1,79 @@
+#include "RollingBuffer.h"
+
+#include <stdint.h>
+
+typedef struct RollingBuffer
+{
+	void **array;
+	uint16_t length;
+
+	uint16_t head;
+	uint16_t tail;
+
+	// uint32_t sum;
+	uint8_t isFull;
+} RollingBuffer;
+
+RollingBuffer *RollingBuffer_construct(void **array, uint16_t length)
+{
+	RollingBuffer *buffer = malloc(sizeof(RollingBuffer));
+	if (buffer == NULL)
+	{
+		// malloc error
+		return NULL;
+	}
+
+	buffer->array = array;
+	buffer->length = length;
+	buffer->head = 0;
+	buffer->tail = 0;
+	buffer->isFull = 0;
+
+	return buffer;
+}
+
+void RollingBuffer_destroy(RollingBuffer *buffer)
+{
+	if (buffer == NULL)
+	{
+		return;
+	}
+
+	free(buffer);
+}
+
+void RollingBuffer_push(RollingBuffer *buffer, void *data)
+{
+	if (is_buffer_full(buffer))
+	{
+		// buffer->sum -= buffer->array[buffer->head++];
+		buffer->head++;
+		buffer->head %= buffer->length;
+	}
+	else if (((buffer->tail - buffer->head) + 1) == buffer->length)
+	{
+		buffer->isFull = 1;
+	}
+
+	// buffer->sum += data;
+
+	// Add the new element to the tail of the array
+	buffer->array[buffer->tail++] = data;
+	// Wrap the tail around to the beginning of the array if necessary
+	buffer->tail %= buffer->length;
+}
+
+// uint16_t RollingBuffer_get_average(RollingBuffer *buffer)
+// {
+// 	if (!is_buffer_full(buffer))
+// 	{
+// 		return 0;
+// 	}
+
+// 	return buffer->sum * ((float)1 / buffer->length);
+// }
+
+uint8_t RollingBuffer_is_full(RollingBuffer *buffer)
+{
+	return buffer->isFull;
+}
---
+++ firmware/Team01.cydsn/lib/RollingBuffer/RollingBuffer.h
@@ -0,0 +1,50 @@
+#ifndef ROLLING_BUFFER_H_
+#define ROLLING_BUFFER_H_
+
+#include <stdint.h>
+
+// Use an opaque type to prevent direct access to members
+typedef struct RollingBuffer RollingBuffer;
+
+/**
+ * @brief Construct a new rolling buffer on the heap.
+ *
+ * @warning The allocated memory must be freed with a call to `RollingBuffer_destroy()`.
+ *
+ * @param array The array to use for the rolling buffer.
+ * @param length The length of the array.
+ * @return RollingBuffer* A pointer to the created rolling buffer, or `NULL` if there was an error.
+ */
+RollingBuffer *RollingBuffer_construct(void **array, uint16_t length);
+/**
+ * @brief Free the memory allocated to a rolling buffer.
+ *
+ * @param buffer The pointer to the rolling buffer to free.
+ */
+void RollingBuffer_destroy(RollingBuffer *buffer);
+
+/**
+ * @brief Push an element to the tail of the rolling buffer.
+ *
+ * @param buffer The pointer to the rolling buffer.
+ * @param data The pointer to the element to push.
+ */
+void RollingBuffer_push(RollingBuffer *buffer, void *data);
+
+// /**
+//  * @brief Calculate the average of all elements in the rolling buffer.
+//  *
+//  * @param buffer The pointer to the rolling buffer.
+//  * @return uint16_t The average value of the rolling buffer.
+//  */
+// uint16_t RollingBuffer_get_average(RollingBuffer *buffer);
+
+/**
+ * @brief Check if the rolling buffer is full.
+ *
+ * @param buffer The pointer to the rolling buffer.
+ * @return 1 If the rolling buffer is full.
+ */
+uint8_t RollingBuffer_is_full(RollingBuffer *buffer);
+
+#endif
