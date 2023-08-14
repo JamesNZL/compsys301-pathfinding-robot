d14c374b054b77fe6db3b9225ed020bb6b7d721f
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-08-14 15:32:14 +1200

    feat: :sparkles: Created flag for control loop data ready

---
+++ firmware/CS301_Class.cydsn/commands.c
@@ -0,0 +1,25 @@
+#include "commands.h"
+
+#include <stdint.h>
+#include <string.h>
+
+CommandEnumeration Commands_match_command(char *command)
+{
+	for (uint8_t i = 0; COMMAND_LIST[i].enumeration != COMMAND_NOT_FOUND; i++)
+	{
+		uint8_t commandLength = strlen(COMMAND_LIST[i].keyword);
+
+		// Check that the incoming command is the correct length
+		if (command[commandLength] != '\0')
+		{
+			return COMMAND_NOT_FOUND;
+		}
+
+		if (strncmp(command, COMMAND_LIST[i].keyword, commandLength) == 0)
+		{
+			return COMMAND_LIST[i].enumeration;
+		}
+	}
+
+	return COMMAND_NOT_FOUND;
+}
---
+++ firmware/CS301_Class.cydsn/movement.c
@@ -0,0 +1,88 @@
+#include "movement.h"
+#include <project.h>
+
+void Movement_set_pwm_1_duty_cycle(uint8_t percent)
+{
+	// set the compare
+	uint8_t compare = Movement_calculate_compare(percent);
+	PWM_1_WriteCompare(compare);
+}
+
+void Movement_set_pwm_2_duty_cycle(uint8_t percent)
+{
+	// set the compare
+	uint8_t compare = Movement_calculate_compare(percent);
+	PWM_2_WriteCompare(compare);
+}
+
+uint8_t Movement_calculate_compare(uint8_t percent)
+{
+	// get max count assuming it is 8 bits
+	uint8_t maxCount = PWM_2_ReadPeriod();
+	uint8_t compare = (float)maxCount * ((float)percent / 100);
+	return compare;
+}
+
+void Movement_set_direction_left(Direction direction)
+{
+	uint8_t currentValue = Dir_Control_Reg_Read();
+	switch (direction)
+	{
+	case DIRECTION_FORWARD:
+	{
+		currentValue |= 1 << LEFT_MOTOR_CR_POS;
+		break;
+	}
+	case DIRECTION_REVERSE:
+	{
+		currentValue &= ~(1 << LEFT_MOTOR_CR_POS);
+		break;
+	}
+	}
+	Dir_Control_Reg_Write(currentValue);
+}
+
+void Movement_set_direction_right(Direction direction)
+{
+	uint8_t currentValue = Dir_Control_Reg_Read();
+	switch (direction)
+	{
+	case DIRECTION_FORWARD:
+	{
+		currentValue |= 1 << RIGHT_MOTOR_CR_POS;
+		break;
+	}
+	case DIRECTION_REVERSE:
+	{
+		currentValue &= ~(1 << RIGHT_MOTOR_CR_POS);
+		break;
+	}
+	}
+	Dir_Control_Reg_Write(currentValue);
+}
+
+void Movement_set_direction(Direction direction)
+{
+	Movement_set_direction_left(direction);
+	Movement_set_direction_right(direction);
+}
+
+// TODO: Change percent to encoder pulses and raise power accordingly
+void Movement_set_speed_left(uint8_t percent)
+{
+	// While (encoder target < encoder reading){
+	// Increase duty cycle
+	//}
+	Movement_set_pwm_1_duty_cycle(percent);
+}
+
+void Movement_set_speed_right(uint8_t percent)
+{
+	Movement_set_pwm_2_duty_cycle(percent);
+}
+
+void Movement_set_speed(uint8_t percent)
+{
+	Movement_set_speed_left(percent);
+	Movement_set_speed_right(percent);
+}
