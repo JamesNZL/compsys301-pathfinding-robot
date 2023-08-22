e7541490428b2cab70650477823597628ba8af07
bcho892 <AKLbc70@gmail.com>
2023-08-22 15:04:17 +1200

    docs: :clown_face: update doxygen for point

--- firmware/Team01.cydsn/lib/Point/point.h
+++ firmware/Team01.cydsn/lib/Point/point.h
@@ -5,13 +5,13 @@
 
 struct Point;
 
-/// @brief
-/// @param x
-/// @param y
+/// @brief Allocates memory for a point representing a point in the maze as (x,y)
+/// @param x the x-coordinates (cols) ZERO INDEXED
+/// @param y the y-coordinates (rows) ZERO INDEXED
 /// @return
 Point *Point_create(uint8_t x, uint8_t y);
 
-/// @brief
+/// @brief Frees the memory occupied by a point
 /// @param point
 /// @return
 void *Point_destroy(*Point point);
