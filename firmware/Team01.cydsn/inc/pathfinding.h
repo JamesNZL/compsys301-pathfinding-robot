e9543fc4fe26ac428a718fc8f5314af200cea3cb
bcho892 <AKLbc70@gmail.com>
2023-08-22 15:25:22 +1200

    feat: :sparkles: start working on pathfinding module

--- firmware/Team01.cydsn/inc/pathfinding.h
+++ firmware/Team01.cydsn/inc/pathfinding.h
@@ -1,5 +1,14 @@
 #ifndef PATHFINDING_H_
 #define PATHFINDING_H_
+#include "../lib/Point/Point.h"
 #include "../lib/Queue/Queue.h"
+#include <stdbool.h>
+
+#define PATHFINDING_MAZE_WIDTH			69
+#define PATHFINDING_MAZE_HEIGH			69
+#define PATHFINDING_STARTING_INDEX		0
+#define PATHFINDING_POSSIBLE_DIRECTIONS 4
+
+bool Pathfinding_coordinates_in_bounds(uint8_t x, uint8_t y);
 
 #endif
---
+++ firmware/Team01.cydsn/src/pathfinding.c
@@ -0,0 +1,4 @@
+#include "pathfinding.h"
+
+const static int8_t dY[PATHFINDING_POSSIBLE_DIRECTIONS] = { 0, 0, -1, 1 };
+const static int8_t dX[PATHFINDING_POSSIBLE_DIRECTIONS] = { -1, 1, 0, 0 };
