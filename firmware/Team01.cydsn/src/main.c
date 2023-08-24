35d075db7cc3a3540c230d8bdc63b32a667b9353
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-08-24 16:36:43 +1200

    chore: :hammer: Update pin assignments

--- firmware/Team01.cydsn/src/main.c
+++ firmware/Team01.cydsn/src/main.c
@@ -24,8 +24,8 @@ int main()
 
 	Movement_init_decoder_ISR();
 	// Movement_turn_right(90);
-	// Movement_move_mm(2000);
-	Movement_set_M1_pulse(300);
+    Movement_move_mm(2000);
+	// Movement_set_M1_pulse(300);
 
 #ifdef USB_ENABLED
 	USBUART_Start(0, USBUART_5V_OPERATION);
