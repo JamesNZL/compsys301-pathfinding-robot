5c1c84b15bdea9bc89ef368a401505512917355d
bcho892 <AKLbc70@gmail.com>
2023-08-25 15:36:21 +1200

    feat: :clown_face: create bat file to compile and run bfs

--- firmware/Team01.cydsn/tests/test_pathfinding.c
+++ firmware/Team01.cydsn/tests/test_pathfinding.c
@@ -1,9 +1,11 @@
 #include "test_pathfinding.h"
+#include <stdio.h>
 
 int main()
 {
 	Point *start = Point_create(1, 1);
 	Point *end = Point_create(17, 1);
 	Pathfinding_find_shortest_path_bfs(start, end, map);
+	printf("BFS Ran");
 	return 0;
 }
