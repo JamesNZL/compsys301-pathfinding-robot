0617dffc8b31822af6fe5ea7312a09df19fe7214
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-08-25 13:26:44 +1200

    feat: :sparkles: Working lightsense through software timer

--- firmware/Lab_Test.cydsn/main.c
+++ firmware/Lab_Test.cydsn/main.c
@@ -13,12 +13,27 @@
 
 CY_ISR(light_sensed)
 {
+	Timer_checklight_Start();
+    Sensor_Output_1_Write(1);
+	isr_lightsense_Disable();
+}
+
+CY_ISR(check_light)
+{
+	if (!Lightsense_Read())
+	{
+        Sensor_Output_1_Write(0);
+        Timer_checklight_Stop();
+        isr_lightsense_Enable();
+	}
+    Timer_checklight_ReadStatusRegister();
 }
 
 int main(void)
 {
 	CyGlobalIntEnable; /* Enable global interrupts. */
 	isr_lightsense_StartEx(light_sensed);
+	isr_checklight_StartEx(check_light);
 
 	/* Place your initialization/startup code here (e.g. MyInst_Start()) */
 
