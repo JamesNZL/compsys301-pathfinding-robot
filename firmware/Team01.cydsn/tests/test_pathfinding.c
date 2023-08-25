7d2f4cb96cd206b03b18bab5df3959ae6eab4b27
bcho892 <AKLbc70@gmail.com>
2023-08-25 15:47:06 +1200

    test: :clown_face: confirm functionality of point data structure

--- firmware/Team01.cydsn/tests/test_pathfinding.c
+++ firmware/Team01.cydsn/tests/test_pathfinding.c
@@ -1,11 +1,14 @@
 #include "test_pathfinding.h"
+#include <assert.h>
 #include <stdio.h>
 
 int main()
 {
 	Point *start = Point_create(1, 1);
 	Point *end = Point_create(17, 1);
-	Pathfinding_find_shortest_path_bfs(start, end, map);
-	printf("BFS Ran");
+	Queue *path = Pathfinding_find_shortest_path_bfs(start, end, map);
+	Node *first = Queue_peek(path);
+	Point *value = Node_get_value(first);
+	printf("First Node: (%i, %i) \n", Point_get_x(value), Point_get_y(value));
 	return 0;
 }
