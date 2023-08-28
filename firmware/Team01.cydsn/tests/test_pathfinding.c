0a856ddffc046961dc735cd1ea236ab7c497172d
bcho892 <AKLbc70@gmail.com>
2023-08-28 20:48:27 +1200

    test: :clown_face: add second test case

--- firmware/Team01.cydsn/tests/test_pathfinding.c
+++ firmware/Team01.cydsn/tests/test_pathfinding.c
@@ -11,13 +11,18 @@ void teardown(Point *start, Point *end, Stack *path)
 
 int main()
 {
+	Point *start;
+	Point *end;
+	Stack *path;
+	int length;
+
 	printf("Test 1 Started.\n");
-	Point *start = Point_create(1, 1, PATHFINDING_MAZE_WIDTH);
-	Point *end = Point_create(17, 1, PATHFINDING_MAZE_WIDTH);
-	Stack *path = Pathfinding_find_shortest_path_bfs(start, end, map);
+	start = Point_create(1, 1, PATHFINDING_MAZE_WIDTH);
+	end = Point_create(17, 1, PATHFINDING_MAZE_WIDTH);
+	path = Pathfinding_find_shortest_path_bfs(start, end, map);
 	// Print the path
 	printf("The shortest path is: \n");
-	int length = 0;
+	length = 0;
 	while (!Stack_is_empty(path))
 	{
 		length++;
@@ -28,6 +33,23 @@ int main()
 	printf("Length of path is: %i \n", length);
 	assert(length == 21);
 	teardown(start, end, path);
+
 	printf("Test 2 Started.\n");
+	start = Point_create(1, 1, PATHFINDING_MAZE_WIDTH);
+	end = Point_create(17, 13, PATHFINDING_MAZE_WIDTH);
+	path = Pathfinding_find_shortest_path_bfs(start, end, map);
+	// Print the path
+	printf("The shortest path is: \n");
+	length = 0;
+	while (!Stack_is_empty(path))
+	{
+		length++;
+		Node *current = Stack_pop(path);
+		Point *value = Node_get_value(current);
+		printf("(%i, %i) \n", Point_get_x(value), Point_get_y(value));
+	}
+	printf("Length of path is: %i \n", length);
+	assert(length == 21);
+	teardown(start, end, path);
 	return 0;
 }
