f99f08d15d48d312080d46537f787e6e346650c6
bcho892 <AKLbc70@gmail.com>
2023-08-28 19:44:03 +1200

    feat: :poop: change driver test code to print the shortest path

--- firmware/Team01.cydsn/tests/test_pathfinding.c
+++ firmware/Team01.cydsn/tests/test_pathfinding.c
@@ -8,8 +8,13 @@ int main()
 	Point *start = Point_create(1, 1, PATHFINDING_MAZE_WIDTH);
 	Point *end = Point_create(17, 1, PATHFINDING_MAZE_WIDTH);
 	Stack *path = Pathfinding_find_shortest_path_bfs(start, end, map);
-	Node *first = Stack_peek(path);
-	Point *value = Node_get_value(first);
-	printf("First Node: (%i, %i) \n", Point_get_x(value), Point_get_y(value));
+	// Print the path
+	printf("The shortest path is: \n");
+	while (!Stack_is_empty(path))
+	{
+		Node *current = Stack_pop(path);
+		Point *value = Node_get_value(current);
+		printf("(%i, %i) \n", Point_get_x(value), Point_get_y(value));
+	}
 	return 0;
 }
