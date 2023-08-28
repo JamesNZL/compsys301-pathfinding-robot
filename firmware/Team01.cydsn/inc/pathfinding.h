055048c52b1fdea5758814d3a6a714d05c27085a
bcho892 <AKLbc70@gmail.com>
2023-08-28 19:16:27 +1200

    docs: :poop: add doxygen comments to pathfinding functions

--- firmware/Team01.cydsn/inc/pathfinding.h
+++ firmware/Team01.cydsn/inc/pathfinding.h
@@ -23,10 +23,20 @@ typedef enum Actions
 uint8_t Pathfinding_coordinates_in_bounds(uint8_t x, uint8_t y);
 
 Node *Pathfinding_create_node(Point *point);
-
+/// @brief Returns a stack of points to represent the shortest path, with the top of the stack being the first node
+/// @param start A Point representing the starting position in the maze
+/// @param end  A Point represetning the destination position of the maze
+/// @param maze 2D array representing the maze
+/// @return
 Stack *
 Pathfinding_find_shortest_path_bfs(Point *start, Point *end, uint8_t maze[PATHFINDING_MAZE_HEIGHT][PATHFINDING_MAZE_WIDTH]);
 
-void *Pathfinding_build_stack_from_pred(Stack *stack, uint16_t[PATHFINDING_MAZE_HEIGHT * PATHFINDING_MAZE_WIDTH], Point *start, Point *end);
+/// @brief Modifies a stack reference using a pred array to construct the shortest path stack
+/// @param stack Reference to stack to be modified
+/// @param pred The predecessor array
+/// @param start A Point representing the starting position in the maze
+/// @param end  A Point represetning the destination position of the maze
+/// @return
+void *Pathfinding_build_stack_from_pred(Stack *stack, uint16_t[PATHFINDING_MAZE_HEIGHT * PATHFINDING_MAZE_WIDTH] pred, Point *start, Point *end);
 
 #endif
