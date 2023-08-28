41823ff49582ed889bd2f69abffb9df754b67adf
bcho892 <AKLbc70@gmail.com>
2023-08-28 19:59:40 +1200

    fix: :poop: fix logic error in neighbour search

--- firmware/Team01.cydsn/src/pathfinding.c
+++ firmware/Team01.cydsn/src/pathfinding.c
@@ -7,14 +7,20 @@ Stack *Pathfinding_find_shortest_path_bfs(Point *start, Point *end, uint8_t maze
 {
 	// predecessor array to store shortest path
 	uint16_t pred[PATHFINDING_MAZE_HEIGHT * PATHFINDING_MAZE_WIDTH];
+	for (int i = 0; i < PATHFINDING_MAZE_HEIGHT * PATHFINDING_MAZE_WIDTH; ++i)
+	{
+		pred[i] = UINT16_MAX; // initialize to "no predecessor"
+	}
+
+	uint8_t visited[PATHFINDING_MAZE_HEIGHT][PATHFINDING_MAZE_WIDTH] = { 0 };
 	Queue *q = Queue_construct();
 	Queue_append(q, Pathfinding_create_node(start));
-	uint8_t visited[PATHFINDING_MAZE_HEIGHT][PATHFINDING_MAZE_WIDTH];
 	Point *current_point;
 	while (!Queue_is_empty(q))
 	{
 		Node *current_node = Queue_pop(q);
 		current_point = Node_get_value(current_node);
+		printf("(%i,%i)", Point_get_x(current_point), Point_get_y(current_point));
 		if (Point_equal(current_point, end))
 		{
 			break;
@@ -23,7 +29,7 @@ Stack *Pathfinding_find_shortest_path_bfs(Point *start, Point *end, uint8_t maze
 		{
 			uint8_t newX = Point_get_x(current_point) + dX[i];
 			uint8_t newY = Point_get_y(current_point) + dY[i];
-			if (maze[newY][newX] == 0 && Pathfinding_coordinates_in_bounds(newX, newY) && !visited[newY][newX])
+			if (Pathfinding_coordinates_in_bounds(newX, newY) && !visited[newY][newX] && maze[newY][newX] == 0)
 			{
 				Point *neighbour_point = Point_create(newX, newY, PATHFINDING_MAZE_WIDTH);
 				Node *neighbour = Pathfinding_create_node(neighbour_point);
@@ -37,6 +43,7 @@ Stack *Pathfinding_find_shortest_path_bfs(Point *start, Point *end, uint8_t maze
 	}
 	Stack *stack = Stack_construct();
 	Pathfinding_build_stack_from_pred(stack, pred, start, end);
+	Queue_destroy(q);
 	return stack;
 }
 
@@ -46,14 +53,17 @@ void Pathfinding_build_stack_from_pred(Stack *stack, uint16_t pred[PATHFINDING_M
 	uint16_t current_index = Point_get_1d(end);
 	// use starting point to know if we've reached the end
 	uint16_t start_index = Point_get_1d(start);
-	Node *end_node = Node_create(end);
-	Stack_push(stack, end_node);
-
-	while (current_index != start_index)
+	while (current_index != UINT16_MAX && current_index != start_index)
 	{
-		current_index = pred[current_index];
 		Node *current_node = Node_create(Point_create_from_1d(current_index, PATHFINDING_MAZE_WIDTH));
 		Stack_push(stack, current_node);
+		current_index = pred[current_index];
+	}
+	// push the start node
+	if (current_index == start_index)
+	{
+		Node *start_node = Node_create(Point_create_from_1d(start_index, PATHFINDING_MAZE_WIDTH));
+		Stack_push(stack, start_node);
 	}
 }
 
