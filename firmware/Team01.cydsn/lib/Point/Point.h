00df8c53cb4880ceb358fc98f6cd8de7ec2786eb
bcho892 <bcho892@aucklanduni.ac.nz>
2023-08-31 17:23:33 +1200

    docs: :clown_face: change format of doxygen for point

--- firmware/Team01.cydsn/lib/Point/Point.h
+++ firmware/Team01.cydsn/lib/Point/Point.h
@@ -5,30 +5,66 @@
 
 typedef struct Point Point;
 
-/// @brief Allocates memory for a point representing a point in the maze as (x,y)
-/// @param x the x-coordinates (cols) ZERO INDEXED
-/// @param y the y-coordinates (rows) ZERO INDEXED
-/// @param COLUMNS the width of the maze which the point resides
-/// @return
+/**
+ * @brief Represents a point in a maze with (x, y) coordinates.
+ */
+struct Point
+{
+	uint8_t x; /**< The x-coordinate (cols) - ZERO INDEXED */
+	uint8_t y; /**< The y-coordinate (rows) - ZERO INDEXED */
+};
+
+/**
+ * @brief Allocates memory for a point representing a location in the maze as (x, y).
+ * @param x The x-coordinate (cols) - ZERO INDEXED
+ * @param y The y-coordinate (rows) - ZERO INDEXED
+ * @param COLUMNS The width of the maze in which the point resides
+ * @return A pointer to the created Point
+ */
 Point *Point_create(uint8_t x, uint8_t y, uint8_t COLUMNS);
 
-/// @brief Allocates memory for a point representing a point in the maze as (x,y) given a zero-indexed 1d index
-/// @param index the 1d index (zero-indexed) of the point
-/// @param COLUMNS the width of the maze which the point resides
-/// @return
+/**
+ * @brief Allocates memory for a point representing a location in the maze as (x, y),
+ *        given a zero-indexed 1D index.
+ * @param index The 1D index (zero-indexed) of the point
+ * @param COLUMNS The width of the maze in which the point resides
+ * @return A pointer to the created Point
+ */
 Point *Point_create_from_1d(uint16_t index, uint8_t COLUMNS);
 
-/// @brief Frees the memory occupied by a point
-/// @param point
-/// @return
+/**
+ * @brief Frees the memory occupied by a point.
+ * @param point The point to be destroyed
+ */
 void Point_destroy(Point *point);
 
-/// @brief Checks if two points are equal
-/// @param point1
-/// @param point2
-/// @return 1 (TRUE) if the nodes are equal or 0 (FALSE) if they are not
+/**
+ * @brief Checks if two points are equal.
+ * @param point1 The first point
+ * @param point2 The second point
+ * @return 1 (TRUE) if the points are equal, 0 (FALSE) otherwise
+ */
 uint8_t Point_equal(Point *point1, Point *point2);
+
+/**
+ * @brief Gets the x-coordinate of a point.
+ * @param point The point
+ * @return The x-coordinate of the point
+ */
 uint8_t Point_get_x(Point *point);
+
+/**
+ * @brief Gets the y-coordinate of a point.
+ * @param point The point
+ * @return The y-coordinate of the point
+ */
 uint8_t Point_get_y(Point *point);
+
+/**
+ * @brief Converts a 2D point to a zero-indexed 1D index based on the width of the maze.
+ * @param point The point to convert
+ * @return The corresponding 1D index
+ */
 uint16_t Point_get_1d(Point *point);
+
 #endif
