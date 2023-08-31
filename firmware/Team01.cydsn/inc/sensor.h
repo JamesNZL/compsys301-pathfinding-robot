78b5f579b1dbc3e787ab5b381caf400c4d89e05f
bcho892 <bcho892@aucklanduni.ac.nz>
2023-08-31 21:58:23 +1200

    feat: :clown_face: add boilerplate for sensor header

--- firmware/Team01.cydsn/inc/sensor.h
+++ firmware/Team01.cydsn/inc/sensor.h
@@ -1,4 +1,9 @@
 #ifndef SENSOR_H
 #define SENSOR_H
-
+#define SENSOR_HARDWARE_MODE	 0;
+#define SENSOR_COUNT			 7;
+#define SENSOR_TURN_SENSOR_POS_1 0;
+#define SENSOR_TURN_SENSOR_POS_2 0;
+#define SENSOR_SKEW_SENSOR_1	 0;
+// put isr in this file
 #endif
