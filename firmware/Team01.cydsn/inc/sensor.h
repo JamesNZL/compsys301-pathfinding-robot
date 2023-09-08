a04b0c1999bddda9822a434cd7851adbaea94423
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-09-08 16:51:09 +1200

    feat: :sparkles: ISR complete

--- firmware/Team01.cydsn/inc/sensor.h
+++ firmware/Team01.cydsn/inc/sensor.h
@@ -15,13 +15,15 @@ CY_ISR(light_sensed)
 CY_ISR(check_light)
 {
 	// sensors.sensor1 = Is_White1_Read()
+	// sensors.sensor2 = Is_White2_Read()
+	// ...
 
-	if (!(Is_White1_Read() ||))
+	if (!(Is_White1_Read() || Is_White2_Read() || Is_White3_Read() || Is_White4_Read() || Is_White5_Read() || Is_White6_Read() || Is_White7_Read())) // sensor1 or sensor2 or sensor3 ...
 	{
 		Timer_Light_Check_Stop();
 		isr_lightsense_Enable();
 	}
-	Timer_checklight_ReadStatusRegister();
+	Timer_Light_Check_ReadStatusRegister();
 }
 
 void Sensor_init_sensors();
