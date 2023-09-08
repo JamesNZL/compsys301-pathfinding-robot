92ce2b3c44fe00654b93a8aa5aa37dc41d947bbf
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-09-08 16:55:35 +1200

    feat: :sparkles: Code for 7 light sensors partially done
    Need to add response like LED or turning.

--- firmware/Team01.cydsn/src/sensor.c
+++ firmware/Team01.cydsn/src/sensor.c
@@ -16,6 +16,7 @@ CY_ISR(check_light)
 
 	if (!(Is_White1_Read() || Is_White2_Read() || Is_White3_Read() || Is_White4_Read() || Is_White5_Read() || Is_White6_Read() || Is_White7_Read())) // sensor1 or sensor2 or sensor3 ...
 	{
+		// Safeguard
 		Timer_Light_Check_Stop();
 		isr_lightsense_Enable();
 	}
