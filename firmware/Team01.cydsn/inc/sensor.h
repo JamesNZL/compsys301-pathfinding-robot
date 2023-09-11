5d34ebb92caadbc2b42cb932b8f2133e57abf9c3
bcho892 <AKLbc70@gmail.com>
2023-09-11 20:42:52 +1200

    refactor: :poop: remove unnecessary defines

--- firmware/Team01.cydsn/inc/sensor.h
+++ firmware/Team01.cydsn/inc/sensor.h
@@ -2,9 +2,6 @@
 #define SENSOR_H
 #define SENSOR_HARDWARE_MODE		0
 #define SENSOR_COUNT				7
-#define SENSOR_TURN_SENSOR_POS_1	0
-#define SENSOR_TURN_SENSOR_POS_2	0
-#define SENSOR_SKEW_SENSOR_1		0
 #define SENSOR_DEFAULT_BIAS_VOLTAGE 1.5F
 
 #include "common.h"
