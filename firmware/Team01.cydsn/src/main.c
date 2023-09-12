ec03b307b1c6789a08840f68be691e4f12552564
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-09-12 13:05:13 +1200

    feat: :sparkles: Tried working set pulse for 2, works better following

--- firmware/Team01.cydsn/src/main.c
+++ firmware/Team01.cydsn/src/main.c
@@ -61,8 +61,8 @@ int main()
 			}
 
 			int8 pulseError1 = MOVEMENT_TPULSE_1 / 100 - MOVEMENT_APPARENT_PULSE_1;
-			// int8 pulseError2 = MOVEMENT_TPULSE_2 / 100 - MOVEMENT_APPARENT_PULSE_2;
-			int8 pulseError2 = MOVEMENT_APPARENT_PULSE_1 - MOVEMENT_APPARENT_PULSE_2;
+			int8 pulseError2 = MOVEMENT_TPULSE_2 / 100 - MOVEMENT_APPARENT_PULSE_2;
+			// int8 pulseError2 = MOVEMENT_APPARENT_PULSE_1 - MOVEMENT_APPARENT_PULSE_2;
 
 			// static char entryy[256];
 			// sprintf(entryy, "ERR: %d\n;\tTGT2: %d\n;\tM1ENC: %d, M2ENC %d\n\n", pulseError2, MOVEMENT_TPULSE_2, MOVEMENT_APPARENT_PULSE_1, MOVEMENT_APPARENT_PULSE_2);
