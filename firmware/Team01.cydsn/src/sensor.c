d809f0c96f7b9f2a46fede11fcf49bb468f3116d
bcho892 <AKLbc70@gmail.com>
2023-09-16 15:27:36 +1200

    fix: :poop: fix error in function name

--- firmware/Team01.cydsn/src/sensor.c
+++ firmware/Team01.cydsn/src/sensor.c
@@ -63,7 +63,7 @@ bool Sensor_is_on_all_turn_intersection()
 	return !Sensor_turnLeft.status && !Sensor_turnRight.status;
 }
 
-SensorActions Sensor_determine_actions()
+SensorActions Sensor_determine_action()
 {
 	uint8 bitfield = ((Sensor_skewCenter.status << 6)
 		| (Sensor_skewFrontLeft.status << 5)
