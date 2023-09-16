2beada13e3b7427ad94c492ea90c5381efe0bb9a
James <james@jamesnzl.xyz>
2023-09-16 20:04:55 +1200

    fix(firmware): :bug: include `cytypes.h` to get `uint16` (#56)

--- firmware/Team01.cydsn/inc/battery.h
+++ firmware/Team01.cydsn/inc/battery.h
@@ -1,6 +1,8 @@
 #ifndef BATTERY_H_
 #define BATTERY_H_
 
+#include <cytypes.h>
+
 #define BATTERY_COUNT	 6
 #define BATTERY_FULL_MV	 1400 * BATTERY_COUNT
 #define BATTERY_EMPTY_MV 1100 * BATTERY_COUNT
