e8cd03fc7625c51f910579603c7c3fee993b0511
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-09-25 15:30:06 +1300

    feat: :sparkles: Changed order of the skew boosting to be correct

--- firmware/Team01.cydsn/src/movement.c
+++ firmware/Team01.cydsn/src/movement.c
@@ -200,8 +200,8 @@ void Movement_skew_correct(Direction direction)
 	{
 	case DIRECTION_LEFT:
 	{
-		Movement_LSB = Movement_currentSpeed * (MOVEMENT_SKEW_CORRECTION_FACTOR/100);
-		Movement_RSB = 0;
+		Movement_RSB = Movement_currentSpeed * (MOVEMENT_SKEW_CORRECTION_FACTOR / 100);
+		Movement_LSB = 0;
 		// FLAG_SET(FLAGS, FLAG_DIRECTIONAL_BIAS);
 		// Movement_set_M2_pulse_target((Movement_currentSpeed * (100 + MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100);
 		// Movement_set_M1_pulse_target(Movement_currentSpeed);
@@ -209,8 +209,8 @@ void Movement_skew_correct(Direction direction)
 	}
 	case DIRECTION_RIGHT:
 	{
-		Movement_RSB = Movement_currentSpeed * (MOVEMENT_SKEW_CORRECTION_FACTOR/100);
-		Movement_LSB = 0;
+		Movement_LSB = Movement_currentSpeed * (MOVEMENT_SKEW_CORRECTION_FACTOR / 100);
+		Movement_RSB = 0;
 		// FLAG_CLEAR(FLAGS, FLAG_DIRECTIONAL_BIAS);
 		// Movement_set_M1_pulse_target((Movement_currentSpeed * (100 + MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100);
 		// Movement_set_M2_pulse_target(Movement_currentSpeed);
