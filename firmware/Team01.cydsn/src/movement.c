11c13fc39777eb7ddea7f46aac4b5b49d25c07af
wolfuoa <100742628+wolfuoa@users.noreply.github.com>
2023-09-27 13:43:58 +1300

    refactor: :fire: Removed old comments - These ones are for negative skew - subtracting from motors instead of adding

--- firmware/Team01.cydsn/src/movement.c
+++ firmware/Team01.cydsn/src/movement.c
@@ -165,22 +165,16 @@ void Movement_skew_correct(Direction direction)
 	{
 	case DIRECTION_LEFT:
 	{
-		Movement_rightSkewBoost = MOVEMENT_SKEW_NUMERIC_PULSES; //(Movement_currentSpeed * (100 + MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100;
+		Movement_rightSkewBoost = MOVEMENT_SKEW_NUMERIC_PULSES;
 		Movement_leftSkewBoost = 0;
-		// FLAG_SET(FLAGS, FLAG_DIRECTIONAL_BIAS);
-		// Movement_set_M1_pulse_target((Movement_currentSpeed * (100 - MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100);
-		// Movement_set_M2_pulse_target(Movement_currentSpeed);
 		Movement_set_M2_pulse_target((Movement_currentSpeed * (100 + MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100);
 		Movement_set_M1_pulse_target(Movement_currentSpeed);
 		break;
 	}
 	case DIRECTION_RIGHT:
 	{
-		Movement_leftSkewBoost = MOVEMENT_SKEW_NUMERIC_PULSES; // (Movement_currentSpeed * (100 + MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100;
+		Movement_leftSkewBoost = MOVEMENT_SKEW_NUMERIC_PULSES;
 		Movement_rightSkewBoost = 0;
-		// FLAG_CLEAR(FLAGS, FLAG_DIRECTIONAL_BIAS);
-		// Movement_set_M2_pulse_target((Movement_currentSpeed * (100 - MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100);
-		// Movement_set_M1_pulse_target(Movement_currentSpeed);
 		Movement_set_M1_pulse_target((Movement_currentSpeed * (100 + MOVEMENT_SKEW_CORRECTION_FACTOR - Movement_skewDamperFactor)) / 100);
 		Movement_set_M2_pulse_target(Movement_currentSpeed);
 		break;
